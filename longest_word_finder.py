3abf50136c97e4adfed5ef631f80d3ec838d5adb
anonaba <117132737+anonaba@users.noreply.github.com>
2026-04-26 15:50:03 +0800

    Update longest_word_finder.py

--- longest_word_finder.py
+++ longest_word_finder.py
@@ -6,7 +6,9 @@ def find_longest_word(sentence: str) -> str | None:
     clean_sentence = ""
     longest_word = ""
 
-    # Remove special charater from the input
+     # Remove special charater from the input
+    """ Issue with this loop :  Strings are immutable. This creates a new string every loop"""
+    # 1st approach
     for char in sentence.lower():
         if char not in string.punctuation:
             clean_sentence += char
@@ -17,6 +19,31 @@ def find_longest_word(sentence: str) -> str | None:
             longest_word = word
 
     return longest_word
+
+print(find_longest_word("Python is awesome!@"))
+
+############################## REFACTOR ##############################
+
+def find_longest_word(sentence: str) -> str | None:
+
+    longest_word = ""
+    chars = []
+
+    # Remove special charater from the input
+    # Better than 1st approach
+    for char in sentence.lower():
+        if char not in string.punctuation:
+            chars.append(char)
+
+    clean_sentence = "".join(chars)
+
+    # save the longest word in the longest_word vairable
+    for word in clean_sentence.split():
+        if len(word) > len(longest_word):
+            longest_word = word
+
+    return longest_word
+
 
 print(find_longest_word("Python is awesome!@"))
 
