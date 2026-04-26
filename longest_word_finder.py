b95f7264aca9cc97c10f77a94e14c52d2c0823f8
anonaba <117132737+anonaba@users.noreply.github.com>
2026-04-26 15:29:03 +0800

    Update longest_word_finder.py
    Recoded

--- longest_word_finder.py
+++ longest_word_finder.py
@@ -1,15 +1,23 @@
+import string
+
+
 def find_longest_word(sentence: str) -> str | None:
 
+    clean_sentence = ""
     longest_word = ""
 
-    for word in sentence.split():
+    # Remove special charater from the input
+    for word in sentence.lower():
+        if word not in string.punctuation:
+            clean_sentence += word
+            
+    # save the longest word in the longest_word vairable
+    for word in clean_sentence.split():
         if len(word) > len(longest_word):
             longest_word = word
 
     return longest_word
 
-
-print(find_longest_word("Python is awesome"))
 
 ############################## REFACTOR ##############################
 
